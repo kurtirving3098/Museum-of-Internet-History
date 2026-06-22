4b23778a04e35f1a2dfce9091b9f04676316370d
maikiraval3098 <maikiraval3098@gmail.com>
2026-06-22 20:56:33 +0900

    Added vercel cors

--- server/index.js
+++ server/index.js
@@ -25,7 +25,8 @@ const cors = require("cors");
 const corsOptions = {
     origin: [
         "http://localhost:5173",
-        "http://localhost:4000"
+        "http://localhost:4000",
+        "https://museum-of-internet-history.vercel.app"
     ],
     credentials:         true,
     optionsSuccessStatus: 200
